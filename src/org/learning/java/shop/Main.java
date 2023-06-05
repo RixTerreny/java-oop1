71d86d93479a6496739884e491abf6a5abb95db6
RixTerreny <riccardotrave43@gmail.com>
2023-06-05 15:28:16 +0200

    Part1 ended with bonus

--- src/org/learning/java/shop/Main.java
+++ src/org/learning/java/shop/Main.java
@@ -2,7 +2,15 @@ package org.learning.java.shop;
 
 public class Main {
     public static void main(String[] args) {
-        Prodotto macchina = new Prodotto( "Audi a3", "Va veloce",23344.50);
-        System.out.println("Codice: " + macchina.getCode());
+
+        Prodotto macchina1 = new Prodotto( "Audi A3", "Va veloce",23344.50);
+        System.out.printf("Codice prodotto: %08d%n", macchina1.getCode());
+        System.out.println("Nome: " + macchina1.getName());
+        System.out.println("Descrizione: " + macchina1.getDescription());
+        System.out.println("Prezzo senza IVA: " + macchina1.getPrice());
+        System.out.println("Nome esteso: " + macchina1.getExtendedName());
+        System.out.println("Prezzo con IVA: " + macchina1.getFullPrice() + "\n");
+        macchina1.setName();
+        System.out.println("Nome modificato con il setter: " + macchina1.getName());
     }
 }
--- src/org/learning/java/shop/Prodotto.java
+++ src/org/learning/java/shop/Prodotto.java
@@ -8,7 +8,7 @@ public class Prodotto {
     private String name;
     private String description;
     private double price;
-    private final int vat = 22;
+    private final double vat = 1.22;
 
     private int generateCode(){
         return random.nextInt(99999999);
@@ -21,7 +21,42 @@ public class Prodotto {
         this.price = price;
     }
 
+    public String getExtendedName(){
+        return this.name +"-"+ this.code;
+    }
+
+    public  double getFullPrice(){
+        return this.price * this.vat;
+    }
+
     public int getCode(){
         return this.code;
     }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public double getPrice(){
+        return this.price;
+    }
+
+
+
+    public void setName(){
+        this.name = "Fiat 500";
+    }
+
+    public void setDescription(){
+        this.description = "Va piano";
+    }
+
+    public void setPrice(){
+        this.price = 12200.34;
+    }
+
 }
